fb0d58ef6bc1eb2a3e43e1456a4779b5a602484e
ldeus <admin@ldeus.com>
2023-07-18 12:50:51 +0000

    Site updated: 2023-07-18 12:50:50

--- js/tools/ruijie_decode.js
+++ js/tools/ruijie_decode.js
@@ -1,21 +1,16 @@
-function rj_decode(){
+function decode(xlat){
     var pwd_in = document.getElementById("pwd-input").value;
     var enc_pw = pwd_in + '\u0000';
-    var xlat = [9999, 42,   64,   35,   35,   87,   120,  102,  94,   99,   79,   117,  114, 71, 101, 114, 42, 109, 65, 114, 75, 76, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999 ];
       var test_pw ='';
       var seed = parseFloat(enc_pw.slice(0 ,2));
-      //console.log("seed:" + seed);
       var val= 0;
   
             for(var i=2 ;i< enc_pw.length;i++){
-                //console.log(i);
-  
                 if (i%2 ===0 && i>2){
                 seed +=1;
                 var test_pw_char = String.fromCharCode(val ^xlat[seed]);
                 test_pw +=test_pw_char;
                 val=0;
-                //console.log(seed,xlat[seed],test_pw_char);
               }
               
                val *=16
@@ -30,10 +25,14 @@ function rj_decode(){
            }
             
    }
-   alert(test_pw);
-    
-    console.log(test_pw);
+    document.getElementById("pwd-output").innerHTML = test_pw;
     return test_pw;
   }
-  //document.getElementById("decode_password").addEventListener("click", rj_decode);
-  
+  function cisco_decode(){
+    var cisco_xlat= [9999, 9999, 9999, 9999, 9999, 59, 107, 102, 111, 65, 44, 46, 105, 121, 101, 119, 114, 107, 108, 100, 74, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999];
+    decode(cisco_xlat);
+  }
+  function ruijie_decode(){
+    var ruijie_xlat = [9999, 42,   64,   35,   35,   87,   120,  102,  94,   99,   79,   117,  114, 71, 101, 114, 42, 109, 65, 114, 75, 76, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999 ];
+    decode(ruijie_xlat);
+  }
